8d2838540cbeaa404a02b5ec7c3ede22a97ff7d8
markgardie <60879509+markgardie@users.noreply.github.com>
2025-10-25 12:16:55 +0300

    start accounts app

--- book_library_project/book_library_project/settings.py
+++ book_library_project/book_library_project/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts'
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'book_library_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -113,8 +114,20 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
+# URL для доступу до статичних файлів
+STATIC_URL = '/static/'
 
-STATIC_URL = 'static/'
+# Папка для збору статичних файлів у продакшені
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+# Папки, де Django шукає статичні файли під час розробки
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
+
+# Media files (користувацькі завантаження)
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
