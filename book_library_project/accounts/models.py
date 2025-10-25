babe0270bf8f1fd4de04edcc49a435c438f7d725
markgardie <60879509+markgardie@users.noreply.github.com>
2025-10-25 13:29:18 +0300

    add accounts models

--- book_library_project/accounts/models.py
+++ book_library_project/accounts/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
-# Create your models here.
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    bio = models.TextField(max_length=500, blank=True, verbose_name="Біографія")
+    avatar = models.ImageField(upload_to='users/avatars/', null=True, blank=True, verbose_name="Аватар")
+    birth_date = models.DateField(null=True, blank=True, verbose_name="Дата народження")
+    favorite_genre = models.CharField(max_length=50, blank=True, verbose_name="Улюблений жанр")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = "Профіль користувача"
+        verbose_name_plural = "Профілі користувачів"
+
+    def __str__(self):
+        return f"Профіль {self.user.username}"
+    
+
+@receiver(post_save, sender = User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user = instance)
+
+@receiver(post_save, sender = User)
+def save_user_profile(sender, instance, **kwargs):
+    UserProfile.objects.save(user = instance)
--- book_library_project/accounts/views.py
+++ book_library_project/accounts/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
-
-# Create your views here.
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from django.contrib.auth.views import LoginView as BaseLoginView, LogoutView as BaseLogoutView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import CreateView, DetailView, UpdateView
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.contrib import messages
+from .models import UserProfile
+from .forms import UserForm, UserProfileForm
